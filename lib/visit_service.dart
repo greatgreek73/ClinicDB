2142abba080f2ae1132a7b8e9eee7b9aa2546219
greatgreek73 <33379483+greatgreek73@users.noreply.github.com>
2023-12-09 19:07:22 +0300

    Delete lib/visit_service.dart

--- lib/visit_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'visit_model.dart';
-
-class VisitService {
-  static Future<void> addVisit(Visit visit) async {
-    await FirebaseFirestore.instance.collection('visits').add(visit.toMap());
-  }
-}
