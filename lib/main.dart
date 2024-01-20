2380219cfcc8d2434c6de080272d9d1c5ad79317
nikos <nikospopulidis@gmail.com>
2024-01-20 07:10:15 +0300

    20.01.24

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             ImplantationSummaryWidget(),
+            CrownAndAbutmentSummaryWidget(),
             Expanded(
               child: Center(
                 child: Wrap(
@@ -126,8 +127,8 @@ class _ImplantationSummaryWidgetState extends State<ImplantationSummaryWidget> {
         .get()
         .then((snapshot) {
           setState(() {
-            totalTeethCountMonth = snapshot.docs.fold(0, (sum, doc) {
-              List toothNumbers = doc['toothNumber'] ?? [];
+            totalTeethCountMonth = snapshot.docs.fold<int>(0, (int sum, doc) {
+              var toothNumbers = List.from(doc['toothNumber'] ?? []);
               return sum + toothNumbers.length;
             });
           });
@@ -141,8 +142,8 @@ class _ImplantationSummaryWidgetState extends State<ImplantationSummaryWidget> {
         .get()
         .then((snapshot) {
           setState(() {
-            totalTeethCountYear = snapshot.docs.fold(0, (sum, doc) {
-              List toothNumbers = doc['toothNumber'] ?? [];
+            totalTeethCountYear = snapshot.docs.fold<int>(0, (int sum, doc) {
+              var toothNumbers = List.from(doc['toothNumber'] ?? []);
               return sum + toothNumbers.length;
             });
           });
@@ -175,6 +176,92 @@ class _ImplantationSummaryWidgetState extends State<ImplantationSummaryWidget> {
           SizedBox(height: 10),
           Text(
             'Количество зубов за текущий год: $totalTeethCountYear',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CrownAndAbutmentSummaryWidget extends StatefulWidget {
+  @override
+  _CrownAndAbutmentSummaryWidgetState createState() => _CrownAndAbutmentSummaryWidgetState();
+}
+
+class _CrownAndAbutmentSummaryWidgetState extends State<CrownAndAbutmentSummaryWidget> {
+  DateTime get firstDateOfMonth => DateTime(DateTime.now().year, DateTime.now().month, 1);
+  DateTime get lastDateOfMonth => DateTime(DateTime.now().year, DateTime.now().month + 1, 0, 23, 59, 59);
+  DateTime get firstDateOfYear => DateTime(DateTime.now().year, 1, 1);
+  DateTime get lastDateOfYear => DateTime(DateTime.now().year, 12, 31, 23, 59, 59);
+
+  int totalTeethCountMonth = 0;
+  int totalTeethCountYear = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    calculateStatistics();
+  }
+
+  void calculateStatistics() {
+    FirebaseFirestore.instance
+        .collection('treatments')
+        .where('treatmentType', whereIn: ['Коронка', 'Абатмент'])
+        .where('date', isGreaterThanOrEqualTo: firstDateOfMonth)
+        .where('date', isLessThanOrEqualTo: lastDateOfMonth)
+        .get()
+        .then((snapshot) {
+          setState(() {
+            totalTeethCountMonth = snapshot.docs.fold<int>(0, (int sum, doc) {
+              var toothNumbers = List.from(doc['toothNumber'] ?? []);
+              return sum + toothNumbers.length;
+            });
+          });
+        });
+
+    FirebaseFirestore.instance
+        .collection('treatments')
+        .where('treatmentType', whereIn: ['Коронка', 'Абатмент'])
+        .where('date', isGreaterThanOrEqualTo: firstDateOfYear)
+        .where('date', isLessThanOrEqualTo: lastDateOfYear)
+        .get()
+        .then((snapshot) {
+          setState(() {
+            totalTeethCountYear = snapshot.docs.fold<int>(0, (int sum, doc) {
+              var toothNumbers = List.from(doc['toothNumber'] ?? []);
+              return sum + toothNumbers.length;
+            });
+          });
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Количество зубов (Коронка и Абатмент) за текущий месяц: $totalTeethCountMonth',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Количество зубов (Коронка и Абатмент) за текущий год: $totalTeethCountYear',
             style: TextStyle(fontSize: 16),
           ),
         ],
